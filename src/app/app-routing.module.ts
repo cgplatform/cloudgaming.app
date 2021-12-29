e9cb064664c6680834b9698533d4d6e26a727f99
Arthur Pimentel <arthurmoraispimentel7@gmail.com>
2021-12-28 21:25:25 -0300

    adjustment app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,25 +1,25 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
 
 const routes: Routes = [
-  {
-    path: '',
-    loadChildren: () =>
-      import('./modules/internal/internal.module').then(
-        (m) => m.InternalModule
-      ),
-  },
-  {
-    path: '',
-    loadChildren: () =>
-      import('./modules/external/external.module').then(
-        (m) => m.ExternalModule
-      ),
-  }
+    {
+        path: "external",
+        loadChildren: () =>
+            import("./modules/external/external.module").then(
+                (m) => m.ExternalModule
+            )
+    },
+    {
+        path: "internal",
+        loadChildren: () =>
+            import("./modules/internal/internal.module").then(
+                (m) => m.InternalModule
+            )
+    }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule],
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
 export class AppRoutingModule {}
