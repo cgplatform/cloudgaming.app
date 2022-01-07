02aa376eb49f8cd5fd6973ed6b5a0abc182b005f
Arthur Pimentel <arthurmoraispimentel7@gmail.com>
2022-01-06 23:17:16 -0300

    Update component screen register confirmation

--- src/app/modules/external/register/register-confirmation/register-confirmation.component.ts
+++ src/app/modules/external/register/register-confirmation/register-confirmation.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-register-confirmation',
-  templateUrl: './register-confirmation.component.html',
-  styleUrls: ['./register-confirmation.component.scss']
+    selector: "app-register-confirmation",
+    templateUrl: "./register-confirmation.component.html",
+    styleUrls: ["./register-confirmation.component.scss"]
 })
 export class RegisterConfirmationComponent implements OnInit {
+    public close: boolean = false;
 
-  constructor() { }
+    constructor(private router: Router) {}
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {}
 
+    public confirmation() {
+        //TODO alterar rota
+        this.router.navigate(["/profile"]);
+    }
 }
